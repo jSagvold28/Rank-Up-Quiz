2a481e6d8efac40d9a475ab7ad6c95b6b1c032d9
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-14 14:25:07 -0600

    Update multipleChoiceTemplate.lua

--- template/multipleChoiceTemplate.lua
+++ template/multipleChoiceTemplate.lua
@@ -3,13 +3,13 @@ local function queston2()
     print("Multiple Choice")
     print("Type a, b, c, d")
     
-    print("How many horsepower does the Tesla Model 3 have?")
-    
+    print("2: How many horsepower does the Tesla Model 3 have?")
     print("a: 400, b: 480, c: 120, d: 1020")
+    print("ANSWER IN UPPECRASE")
     
     local answer = io.read()
     
-    if answer == "b" then
+    if answer == "B" then
         print("Correct")
         print("+ 5 points")
     else 
