317839749d40ddfb562df2c846b039c4838d6e66
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-14 09:46:50 -0600

    Create trueOrFalse.lua

---
+++ game/trueOrFalse.lua
@@ -0,0 +1,17 @@
+local function question1()
+
+    print("True or False")
+    print("Type T for true and F for false.")
+
+    print("1: The average person drives 14,263 a year")
+
+    local answer = io.read()
+
+    if answer == "T" then
+        print("Correct")
+        print("+5 points")
+    else
+        print("Sorry, incorrect.")
+    end
+end
+question1()
